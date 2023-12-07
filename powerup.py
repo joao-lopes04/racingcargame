3494733effa64b5b7c811accbd64b76aefcf9d82
diogomsilva15 <153184946+diogomsilva15@users.noreply.github.com>
2023-12-07 11:33:58 +0000

    Update powerup.py

--- powerup.py
+++ powerup.py
@@ -1,3 +1,13 @@
 
 
 class PowerUp():
+
+  self.PUslow = pygame.image.load("img/")
+  self.PU50 = pygame.image.load("img/")
+  self.PUinvencibility = pygame.image.load("img/")
+  self.PUshrink = pygame.image.load("img/)
+                            
+  self.PUslow_mask = pygame.mask.from_surface(PUslow)
+  self.PU50_mask = pygame.mask.from_surface(PU50)
+  self.PUinvencibility_mask = pygame.mask.from_surface(PUinvencibility)
+  self.PUshrink_mask = pygame.mask.from_surface(PUshrink)
