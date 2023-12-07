603b3a6ad55f55fd8cef84bf03283b042036724d
diogomsilva15 <153184946+diogomsilva15@users.noreply.github.com>
2023-12-07 12:15:45 +0000

    Update powerup.py

--- powerup.py
+++ powerup.py
@@ -9,17 +9,30 @@ class PowerUp:
 
 
 class PUslow(PowerUp):
-  def apply(self, player):
+  def __init__():
+    super().__init__()
+    self.image = pygame.image.load("img/SlowPowerUp")
+    self.pu_duration = 420
     
+  def apply(self, ):
+    self.enemy_velocity = 2
 
 
 class PU50(PowerUp):
-
+  def __init__():
+    super().__init__()
+    self.image = pygame.image.load("img/+50PowerUp")
 
 
 class PUshrink(PowerUp):
-
+  def __init__():
+    super().__init__()
+    self.image = pygame.image.load("img/ShrinkPowerUp")
+    self.pu_duration = 420
 
 
 class PUinvencibility(PowerUp):
-  
+  def __init__():
+    super().__init__()
+    self.image = pygame.image.load("img/InvencibilityPowerUp")
+    self.pu_duration = 420
